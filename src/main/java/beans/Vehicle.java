9b726c5c3698197a7f44963c361965db2bcdfdde
Uday <budayteja009@gmail.com>
2025-03-09 11:49:29 +0530

    Add Beans

--- src/main/java/beans/Vehicle.java
+++ src/main/java/beans/Vehicle.java
@@ -1,8 +1,26 @@
 package beans;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Vehicle {
 
     private String name;
+
+    // context.close() will call the destroy method
+    // @PreDestroy annotation is used to perform clean-up operations before the bean is removed from the container.
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Destroy method called");
+    }
+
+    // @PostConstruct annotation is used to initialize the bean after the bean has been constructed.
+    @PostConstruct
+    public void initialize() {
+        this.name = "Honda City";
+    }
 
     public String getName() {
         return name;
@@ -10,6 +28,10 @@ public class Vehicle {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void printHello() {
+        System.out.println("Hello World");
     }
 
 }
