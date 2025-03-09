c72c468177a4562bed0ca1984f2e9464facc0672
Uday <budayteja009@gmail.com>
2025-03-09 12:00:08 +0530

    Add Beans using registerBean()

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
 import beans.Vehicle;
 import config.ProjectConfig;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Random;
+import java.util.function.Supplier;
 
 public class Application {
 
@@ -8,12 +12,47 @@ public class Application {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Vehicle vehicle = context.getBean(Vehicle.class);
-        System.out.println(vehicle.getName());
+        Vehicle volkswagen = new Vehicle();
+        volkswagen.setName("Volkswagen");
+        Supplier<Vehicle> volkswagenSupplier = () -> volkswagen;
 
-        vehicle.printHello();
-        context.close();
+        Supplier<Vehicle> audiSupplier = () -> {
+            Vehicle audi = new Vehicle();
+            audi.setName("Audi");
+            return audi;
+        };
 
+        Random random = new Random();
+        int randomNumber = random.nextInt(10);
+        System.out.println("Random number: " + randomNumber);
+
+        if (randomNumber % 2 == 0) {
+            context.registerBean("volkswagen", Vehicle.class, volkswagenSupplier);
+            System.out.println(context.getBean("volkswagen"));
+        } else {
+            context.registerBean("audi", Vehicle.class, audiSupplier);
+            System.out.println(context.getBean("audi"));
+        }
+
+        Vehicle volksVehicle = null;
+        Vehicle audiVehicle = null;
+        try {
+            volksVehicle = context.getBean("volkswagen", Vehicle.class);
+        } catch (NoSuchBeanDefinitionException e) {
+            System.out.println("Bean volkswagen not found");
+        }
+
+        try {
+            audiVehicle = context.getBean("audi", Vehicle.class);
+        } catch (NoSuchBeanDefinitionException e) {
+            System.out.println("Bean audi not found");
+        }
+
+        if (volksVehicle != null) {
+            volksVehicle.printHello();
+        } else if (audiVehicle != null) {
+            audiVehicle.printHello();
+        }
     }
 
 }
--- src/main/java/beans/Vehicle.java
+++ src/main/java/beans/Vehicle.java
@@ -1,26 +1,8 @@
 package beans;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Vehicle {
 
     private String name;
-
-    // context.close() will call the destroy method
-    // @PreDestroy annotation is used to perform clean-up operations before the bean is removed from the container.
-    @PreDestroy
-    public void destroy() {
-        System.out.println("Destroy method called");
-    }
-
-    // @PostConstruct annotation is used to initialize the bean after the bean has been constructed.
-    @PostConstruct
-    public void initialize() {
-        this.name = "Honda City";
-    }
 
     public String getName() {
         return name;
